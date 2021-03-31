0fad6989ae870cf0fd13160ac43b7f3c4daa7b59
Erick Bustos <A01378966@itesm.mx>
2021-03-30 19:18:47 -0600

    Comentarios sobre código y eliminación de assets irrelevantes

--- IT Llama a casa/Assets/Scripts/Arrastrar.cs
+++ IT Llama a casa/Assets/Scripts/Arrastrar.cs
@@ -5,32 +5,36 @@ using System.Diagnostics.Tracing;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/*
+ * Permite arrastrar objetos en pantalla con el uso del mouse
+ * Autor: Erick Bustos
+ */
+
 public class Arrastrar : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] private Canvas canvas;
-
-
     private RectTransform rectTransform;
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
     }
 
+    //Comienza el arrastre
     public void OnBeginDrag(PointerEventData eventData)
     {
     }
-
+    //Durante el arrastre
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        //Mueve el objeto seleccionado hacía la posición del mouse, corrige el tomando en cuenta la escala del canvas
         rectTransform.anchoredPosition += eventData.delta
                                           / canvas.scaleFactor;
     }
-
+    //Al finalizar el arrastre
     public void OnEndDrag(PointerEventData eventData)
     {
     }
-
+    //Cuando se selecciona el objeto con el mouse
     public void OnPointerDown(PointerEventData eventData)
     {
     }
