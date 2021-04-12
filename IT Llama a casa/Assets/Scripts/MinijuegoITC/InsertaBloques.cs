81fb08426b0ab5dbb9a063f77274e00c95bfc8b2
Erick Bustos <A01378966@itesm.mx>
2021-04-12 14:54:08 -0500

    Actualización Minijuego ITC:
    Visualización de la inserción de bloques de código completada

--- IT Llama a casa/Assets/Scripts/MinijuegoITC/InsertaBloques.cs
+++ IT Llama a casa/Assets/Scripts/MinijuegoITC/InsertaBloques.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
+
 /*
  * Permite al usuario agregar bloques de código haciendo uso de los botones
  * en la parte inferior.
@@ -9,14 +12,30 @@ using UnityEngine;
  */
 public class InsertaBloques : MonoBehaviour
 {
+    // Bloque que se va a instanciar
     public GameObject bloque;
+    // Padre al que se va asignar el nuevo objeto
     public GameObject parent;
-    public static int numBloque;
+    // Número asignado a la línea de código
+    public static int numBloque = 0;
+    // Referencia al botón necesaria para extraer texto de los campos inputfield
+    public GameObject boton;
     
     public void AgregarBloque()
     {
         //Crea una nueva instancia de un cierto bloque de programación
         GameObject bloqueClon = Instantiate(bloque, parent.transform);
+        numBloque += 1;
+        // Asigno el número correspondiente a la línea de código
+        bloqueClon.GetComponentInChildren<Text>().text = numBloque.ToString();
         
+        // En caso de que se tengan estas etiquetas debemos preservar la información en el inputfield correspondiente
+        if (boton.CompareTag("Avanzar") | boton.CompareTag("Frenar"))
+        {
+            //  Copiar Número
+            bloqueClon.GetComponentsInChildren<Text>()[2].text = boton.GetComponentsInChildren<Text>()[2].text;
+            // Eliminar número ingresado en botón
+            boton.GetComponentInChildren<InputField>().text = "";
+        }
     }
 }
