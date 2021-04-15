d0f14501c6c0b109085941d8d9082d274634527c
A01750170 <A01750170@itesm.mx>
2021-04-15 14:54:24 -0500

    Nivel del agua y enemigos
    *Se optimizo el código de los enemigos. Ahora todos los enemigos usan el mismo codigo
    *Se añadieron los enemigos en el nivel del agua.

---
+++ IT Llama a casa/Assets/Scripts/Enemigos/movimientoEnemigo.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+Permite que un enemigo se mueva en el eje x,y o ambos.
+Autores: David Rodríguez Fragoso, Edna Jacqueline Zavala Ortega, 
+Erick Alberto Bustos Cruz, Erick Hernández Silva, Israel Sánchez Miranda
+*/
+public class movimientoEnemigo : MonoBehaviour
+{
+    public float velocidadX = 0; //Velocidad en x por defecto
+    public float velocidadY = 0; //Velocidad en y por defecto
+    //nos permite saber cuantos frames se ha movido el enemigo y 
+    //si primero irá a la izq o derecha
+    public float contador = 0; 
+    public bool negativo = true;
+    public int frames;
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer sprRenderer;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        sprRenderer = GetComponent<SpriteRenderer>();  
+    }
+
+    void Update()
+    {   
+        if(contador<frames && !negativo){
+            rigidbody.velocity =new Vector2(velocidadX, velocidadY);
+            contador++;
+            if(velocidadX>0){
+                sprRenderer.flipX = true;
+            }
+        }
+        else if (contador == frames && negativo){
+            contador = 0;
+            negativo = !negativo;
+        }
+        if(contador<frames && negativo){
+            rigidbody.velocity =new Vector2(-1*velocidadX, -1*velocidadY);
+            contador++;
+            if(velocidadX>0){
+                sprRenderer.flipX = false;
+            }
+        }
+        else if (contador == frames && !negativo){
+            contador = 0;
+            negativo = !negativo;
+        }
+    }
+}
