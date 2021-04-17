9b7338593c3c893d5889422d176e1b5c5b36a280
Erick Bustos <A01378966@itesm.mx>
2021-04-17 17:56:20 -0500

    Actualización Minijuego ITC
    Agregamos explosión de la nave y creación de prefab hoyo negro
    
    Co-Authored-By: David Rodriguez Fragoso <42352919+A01748760@users.noreply.github.com>

--- IT Llama a casa/Assets/Scripts/MinijuegoITC/Nave.cs
+++ IT Llama a casa/Assets/Scripts/MinijuegoITC/Nave.cs
@@ -2,7 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * Permite determinar si la nave choca con un enemigo o
+ * si llega al planeta de destino
+ * Autores: Erick Bustos, David Rodriquez
+ */
 public class Nave : MonoBehaviour
 {
     public GameObject botonReiniciar;
@@ -18,10 +22,27 @@ public class Nave : MonoBehaviour
         else
         {
             //EXPLODE
-            botonReiniciar.gameObject.SetActive(true);
+            //Dejar de dibujar la nave
+            GetComponent<SpriteRenderer>().enabled = false;
+            //Activar la explosión
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            // Llamar corrutina
+            StartCoroutine(ContarExplosion());
+
         }
     }
-    
+
+    private IEnumerator ContarExplosion()
+    {
+        // Espera medio segundo y desactiva la explosión
+        yield return new WaitForSeconds(0.5f);
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        // Espera medio segundo y regresa la nave al punto inicial
+        yield return new WaitForSeconds(0.5f);
+        transform.position = new Vector3((float) -8.48,(float) 4.49);
+        transform.rotation =  Quaternion.Euler(0, 0, 0);
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
     
 }
 
