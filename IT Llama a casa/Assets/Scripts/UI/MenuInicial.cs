87e6d77bf8ec76aab1e3db8aab99f61dabd8c06f
Erick Bustos <A01378966@itesm.mx>
2021-04-21 10:56:02 -0500

    Creación de Menús Iniciales
    Se crearon las escenas MenuInicial, MenuIniciarSesión y se modificó la escena MenuPrincipal. Se agregó funcionalidad a algunos de los botones.

---
+++ IT Llama a casa/Assets/Scripts/UI/MenuInicial.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void IniciaSesion()
+    {
+        SceneManager.LoadScene("MenuIniciarSesion");
+    }
+
+    public void Registro()
+    {
+        Application.OpenURL("http://localhost:8080/jugador/formularioRegistro#");
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
