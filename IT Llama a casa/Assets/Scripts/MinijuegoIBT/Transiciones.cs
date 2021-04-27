54bbf6d84dd2718039f4cfeea75bc2860ab50f97
Jacqueline Zavala <a01750480@itesm.mx>
2021-04-27 15:54:47 -0500

    Minijuego IBT
    Avance parcial de los paneles iniciales del minijuego de IBT.

---
+++ IT Llama a casa/Assets/Scripts/MinijuegoIBT/Transiciones.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+//using UnityEngine.SceneManagement;
+
+/*
+* Permite manejar las transiciones de los paneles iniciales del juego.
+* Autores: Jacqueline Zavala e Israel Sánchez.
+*/
+
+
+public class Transiciones : MonoBehaviour
+{
+    public void Contexto(){
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
